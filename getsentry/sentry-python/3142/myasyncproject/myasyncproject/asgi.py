99bb2dee8a13a4c64a91368bd2e78858ba979c70
Daniel Szoke <daniel.szoke@sentry.io>
2024-06-11 16:25:02 +0200

    wip: getsentry/sentry-python#3142 reproduction

---
+++ getsentry/sentry-python/3142/myasyncproject/myasyncproject/asgi.py
@@ -0,0 +1,29 @@
+"""
+ASGI config for myasyncproject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import asyncio
+import os
+
+from django.core.asgi import get_asgi_application
+from django.http import HttpResponse
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myasyncproject.settings")
+
+application = get_asgi_application()
+
+
+async def async_view(request):
+    print("async_view")
+    await asyncio.sleep(0.6)
+    return HttpResponse("Hello, world!")
+
+
+async def error(request):
+    await asyncio.sleep(0.6)
+    raise Exception("error")
